2ac4da36f7c5048ca4e10d6ce1b4f1131ca957ae
mr_zhaojie <mr_zhaojie@163.com>
2018-12-11 18:03:51 +0800

    校验

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@ import 'bootstrap'
 import 'jquery'
 import './index.scss'
 import loginValidate from './js/login'
+import * as my from './js/demo'
+import {
+    multiply
+} from './js/demo'
+import {
+    area,
+    circumference
+} from './js/circle'
+
 
 loginValidate();
+
+console.log(my.firstName)
+console.log(multiply(6, 6))
+console.log(my.multiply(5, 5))
+
+console.log('圆面积：' + area(4));
+console.log('圆周长：' + circumference(14));
---
+++ src/js/demo.js
@@ -0,0 +1,10 @@
+// es6模块学习
+let firstName = 'Michael';
+let lastName = 'Jackson';
+let year = 1958;
+
+export function multiply(x, y) {
+    return x * y;
+};
+
+export {firstName, lastName, year}
