053e4c8cd323d75e272370e61f46ed692fd041d5
mr_zhaojie <mr_zhaojie@163.com>
2018-12-13 21:11:22 +0800

    eslint校验

--- src/js/login.js
+++ src/js/login.js
@@ -2,10 +2,10 @@ export default function loginValidate() {
     let loginForm = document.getElementById('loginForm');
     let loginBtn = document.getElementById('loginBtn');
     let username = loginForm[0];
-    let password = loginForm[1];
+    var password = loginForm[1];
 
     loginBtn.addEventListener('click', event => {
-        if(!username.value || !password.value) {
+        if (!username.value || !password.value) {
             alert('用户名或密码为空');
         }
     })
